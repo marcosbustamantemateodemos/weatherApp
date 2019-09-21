5a363c77d6c4940c5906ec7fda1c24c9b85d3106
Marcos Bustamante Mateo <sevillamarcoss@gmail.com>
2019-09-21 20:57:21 +0200

    Add files via upload

---
+++ precache-manifest.b2141dccfc0684df0d1e9c0b6fc076bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b466835e4d0f58ee674f5554580afb3",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "7eb2e61740a66361001e",
+    "url": "/weatherApp/static/css/main.a8e90c4a.chunk.css"
+  },
+  {
+    "revision": "1292e51d16e6ee8286a7",
+    "url": "/weatherApp/static/js/2.4f6098f5.chunk.js"
+  },
+  {
+    "revision": "7eb2e61740a66361001e",
+    "url": "/weatherApp/static/js/main.0bd01eac.chunk.js"
+  },
+  {
+    "revision": "faa2a4b5f5d8ee2ab091",
+    "url": "/weatherApp/static/js/runtime~main.7d605188.js"
+  }
+]);
